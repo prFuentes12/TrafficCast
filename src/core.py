f31a473dddd04f7a69e32fe406499739940bbfe7
Fernando <fdofuentescarrasco@gmail.com>
2025-04-21 10:37:31 +0200

    Donwload and preprocessing

--- src/core.py
+++ src/core.py
@@ -1,6 +1,28 @@
 import kagglehub
+import os
+import shutil
 
-# Download latest version
+
+
+
+# ============================
+# DOWNLOAD & PREPOCESSING
+# ============================
+
+# Download the latest version of the dataset
 path = kagglehub.dataset_download("rohith203/traffic-volume-dataset")
 
-print("Path to dataset files:", path)
+# Define the path to the 'data' folder outside of 'src'
+data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
+
+# Create the 'data' folder if it doesn't already exist
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+# Move the downloaded files to the 'data' folder
+for file in os.listdir(path):
+    file_path = os.path.join(path, file)
+    if os.path.isfile(file_path):
+        shutil.move(file_path, os.path.join(data_dir, file))
+
+
